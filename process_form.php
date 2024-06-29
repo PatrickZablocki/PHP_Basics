ad97634dc40c4982e066247dfb9700c4783b1223
PatrickZablocki <PatrickZablocki@gmx.de>
2024-06-29 20:29:10 +0200

    PHP Variabeln Lernen

--- process_form.php
+++ process_form.php
@@ -14,5 +14,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Wenn das Formular nicht korrekt abgeschickt wurde, gibt eine Fehlermeldung aus
     echo "Formular wurde nicht korrekt abgeschickt.";
 }
+
+    $echo = "userName: Patrick3103";
+    $echo = "Age: 27";
+// Lokale Variabeln die in der Funktion Geschrieben werden
+    function ausgabe() {
+        $userName= "Patrick3103";
+        echo $name;
+    }
+// Variabeln die AUSERHALB der Fucntion geschrieben werden sind Globale Variabeln
+    $second_Username= "Patrick_31";
+
+    function ausgabe(){
+        // Mit global siehe unten kann ich auch INNERHALB einer Funktion auf globale variabeln zugreifen
+        global $name;
+        echo $name;
+    }
+    // Sinnvoll ist es jedoch diese an Parameter weiterzugeben
+    $triple_name= "Patrick_Z31";
+
+    function ausgabe ($name) {
+        echo $name;
+    }
 ?>
 
