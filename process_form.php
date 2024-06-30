df211e1a151cde8a07c0d941c70a9ab9712d6963
PatrickZablocki <PatrickZablocki@gmx.de>
2024-06-30 15:03:39 +0200

    Heute lerne ich in PHP if else Abfragen

--- process_form.php
+++ process_form.php
@@ -15,26 +15,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Formular wurde nicht korrekt abgeschickt.";
 }
 
-    $echo = "userName: Patrick3103";
-    $echo = "Age: 27";
-// Lokale Variabeln die in der Funktion Geschrieben werden
-    function ausgabe() {
-        $userName= "Patrick3103";
-        echo $name;
-    }
-// Variabeln die AUSERHALB der Fucntion geschrieben werden sind Globale Variabeln
-    $second_Username= "Patrick_31";
+// Lokale Variablen, die in der Funktion geschrieben werden
+function ausgabeLokaleVariable() {
+    $userName = "Patrick3103";
+    echo $userName;
+}
 
-    function ausgabe(){
-        // Mit global siehe unten kann ich auch INNERHALB einer Funktion auf globale variabeln zugreifen
-        global $name;
-        echo $name;
-    }
-    // Sinnvoll ist es jedoch diese an Parameter weiterzugeben
-    $triple_name= "Patrick_Z31";
+// Globale Variablen
+$second_Username = "Patrick_31";
 
-    function ausgabe ($name) {
-        echo $name;
-    }
+function ausgabeGlobaleVariable() {
+    // Mit global kann man innerhalb einer Funktion auf globale Variablen zugreifen
+    global $second_Username;
+    echo $second_Username;
+}
+
+// Sinnvoller ist es jedoch, diese als Parameter zu übergeben
+$triple_name = "Patrick_Z31";
+
+function ausgabeMitParameter($name) {
+    echo $name;
+}
+
+// If-else-Abfrage
+$alter = 27;
+
+if ($alter == 20) {
+    echo "Die Person ist 20 Jahre alt.";
+} else {
+    echo "Die Person ist nicht 20 Jahre alt";
+}
 ?>
-
