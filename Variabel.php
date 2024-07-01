cfbfa3d2473510300588e70c3eff02f935d90e26
PatrickZablocki <PatrickZablocki@gmx.de>
2024-07-01 18:08:02 +0200

    Ich hab nun mal zum Üben das Formular hinzugefügt das ich eine Nachricht bekomme wenn ich es abschicke, desweiteren noch Variabel.php hinzugefügt damit ich da besser differenzieren kann um zu lernen

--- Variabel.php
+++ Variabel.php
@@ -1,20 +1,4 @@
 <?php
-// Überprüft, ob das Formular mit der POST-Methode abgeschickt wurde
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Holt die übermittelten Daten aus dem POST-Array und schützt sie mit htmlspecialchars vor XSS-Angriffen
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // Gibt die übermittelten Daten in HTML-Format aus
-    echo "Name: $name<br>";
-    echo "E-Mail: $email<br>";
-    echo "Nachricht: $message<br>";
-} else {
-    // Wenn das Formular nicht korrekt abgeschickt wurde, gibt eine Fehlermeldung aus
-    echo "Formular wurde nicht korrekt abgeschickt.";
-}
-
 // Lokale Variablen, die in der Funktion geschrieben werden
 function ausgabeLokaleVariable() {
     $userName = "Patrick3103";
